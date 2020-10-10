da9cb54cf06ca4ce3b51fef0dec240b3411b052c
Lương duy huynh <47498502+huynhLuong@users.noreply.github.com>
2020-10-10 17:39:48 +0700

    Update ahihi.c

--- ahihi.c
+++ ahihi.c
@@ -6,23 +6,7 @@
 #define T1 4
 #define T2 8 
 #define DONE 10
-
 int count;
-void *functionCount1();
-void *functionCount2();
-
-void main()
-{
-    pthread_t thread1, thread2;
-    int ret1, ret2;
-    
-    ret1 = pthread_create(&thread1, NULL, &functionCount1, NULL);
-    ret2 = pthread_create(&thread2, NULL, &functionCount2, NULL);
-    if ( 0 != ret1 || 0 != ret2 )
-        perror("False reason: ");
-    pthread_join( thread1, NULL );
-    pthread_join( thread2, NULL );
-}
 
 void *functionCount1()
 {
@@ -47,3 +31,18 @@ void *functionCount2()
             pthread_exit(NULL);
     }
 }
+
+void main()
+{
+    pthread_t thread1, thread2;
+    int ret1, ret2;
+    
+    ret1 = pthread_create(&thread1, NULL, &functionCount1, NULL);
+    ret2 = pthread_create(&thread2, NULL, &functionCount2, NULL);
+    if ( 0 != ret1 || 0 != ret2 )
+        perror("False reason: ");
+    pthread_join( thread1, NULL );
+    pthread_join( thread2, NULL );
+}
+
+
