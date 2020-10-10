b522e68e4f1711b0c74b7ae65a04a198dfcc20bd
Lương duy huynh <47498502+huynhLuong@users.noreply.github.com>
2020-10-10 16:04:55 +0700

    Create ahihi.c

---
+++ ahihi.c
@@ -0,0 +1,26 @@
+void *thread_start(void *args)
+{
+    pthread_exit(NULL);
+}
+
+int main(void)
+{
+    pthread_t thread1;
+    int ret;
+    long count = 0;
+    
+    while (1){
+        if (ret = pthread_create(&thread1, NULL, thread_start, NULL))
+        {
+            perror("Fail reason:");
+            break;
+        }
+        usleep(10);
+        count++;
+        if (count % 10000 == 0)
+        {
+            printf("Number of threads are created: %ld\n", count);
+        }
+    }
+    return 0;
+}
